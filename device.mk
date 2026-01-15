d4891f1b81dcd4cd46a0abbbafe8dda5b0d0a3e8
SonicBSV <sonicbsv@ya.ru>
2026-01-15 07:56:47 +0300

    X00TD: move android.hardware.biometrics.fingerprint@2.1-service to common tree

--- device.mk
+++ device.mk
@@ -13,10 +13,6 @@ $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service-X00TD
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/asus/X00TD/overlay
 
