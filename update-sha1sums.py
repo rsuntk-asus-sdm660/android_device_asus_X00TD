abd7d7f3c5a9667fe956c80afe48e042db8049bf
SonicBSV <sonicbsv@ya.ru>
2026-08-14 15:51:45 +0300

    X00TD: update python scripts for generate blobs

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -1,67 +1,87 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Modified to update SHA1 for all proprietary blobs
 #
 
 import os
 import sys
 from hashlib import sha1
 
-device='X00TD'
-vendor='asus'
+device = 'X00TD'
+vendor = 'asus'
 
-with open('proprietary-files.txt', 'r') as f:
+script_dir = os.path.dirname(os.path.abspath(__file__))
+prop_file = os.path.join(script_dir, 'proprietary-files.txt')
+vendor_path = os.path.normpath(
+    os.path.join(script_dir, '../../../vendor', vendor, device, 'proprietary')
+)
+
+with open(prop_file, 'r', encoding='utf-8') as f:
     lines = f.read().splitlines()
-vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'
-needSHA1 = False
+
+
+def sha1sum(path):
+    h = sha1()
+    with open(path, 'rb') as f:
+        for chunk in iter(lambda: f.read(1024 * 1024), b''):
+            h.update(chunk)
+    return h.hexdigest()
 
 
 def cleanup():
     for index, line in enumerate(lines):
-        # Skip empty or commented lines
-        if len(line) == 0 or line[0] == '#' or '|' not in line:
+        if not line or line.startswith('#') or '|' not in line:
             continue
+        lines[index] = line.split('|', 1)[0]
 
-        # Drop SHA1 hash, if existing
-        lines[index] = line.split('|')[0]
+
+def resolve_blob_path(entry_without_hash):
+    # Examples:
+    # path
+    # path;SYMLINK=...
+    # src:dst
+    # -path
+    # -src:dst;MODULE_SUFFIX=_vendor
+
+    file_spec = entry_without_hash.split(';', 1)[0]
+
+    if file_spec.startswith('-'):
+        file_spec = file_spec[1:]
+
+    candidates = []
+    if ':' in file_spec:
+        src, dst = file_spec.split(':', 1)
+        candidates.append(src)
+        candidates.append(dst)
+    else:
+        candidates.append(file_spec)
+
+    for rel_path in candidates:
+        full_path = os.path.join(vendor_path, rel_path)
+        if os.path.isfile(full_path):
+            return full_path, rel_path
+
+    return None, candidates[0]
 
 
 def update():
     for index, line in enumerate(lines):
-        # Skip empty lines
-        if len(line) == 0:
+        if not line or line.startswith('#'):
             continue
 
-        # Check if we need to set SHA1 hash for the next files
-        if line[0] == '#':
-            needSHA1 = (' - from' in line)
+        entry_without_hash = line.split('|', 1)[0]
+        full_path, shown_path = resolve_blob_path(entry_without_hash)
+
+        if full_path is None:
+            print(f'[MISS] {shown_path}')
             continue
 
-        if needSHA1:
-            # Remove existing SHA1 hash
-            line = line.split('|')[0]
-
-            filePath = line.split(';')[0].split(':')[-1]
-            if filePath[0] == '-':
-                filePath = filePath[1:]
-
-            with open(os.path.join(vendorPath, filePath), 'rb') as f:
-                hash = sha1(f.read()).hexdigest()
-
-            lines[index] = '%s|%s' % (line, hash)
+        digest = sha1sum(full_path)
+        lines[index] = f'{entry_without_hash}|{digest}'
+        print(f'[ OK ] {shown_path}')
 
 
 if len(sys.argv) == 2 and sys.argv[1] == '-c':
@@ -69,5 +89,5 @@ if len(sys.argv) == 2 and sys.argv[1] == '-c':
 else:
     update()
 
-with open('proprietary-files.txt', 'w') as file:
-    file.write('\n'.join(lines) + '\n')
+with open(prop_file, 'w', encoding='utf-8') as f:
+    f.write('\n'.join(lines) + '\n')
